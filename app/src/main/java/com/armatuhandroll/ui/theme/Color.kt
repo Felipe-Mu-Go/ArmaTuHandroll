4608298f8ef4bfb32271eba9c05ecb24fa45ad04
Felipe-Mu-Go <gonzalo.munoz.valencia@gmail.com>
2026-03-09 16:38:01 -0300

    Crear app Android Arma Tu Handroll con Compose

---
+++ app/src/main/java/com/armatuhandroll/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.armatuhandroll.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WarmBlack = Color(0xFF15100D)
+val DeepBrown = Color(0xFF2A1D17)
+val SoftBeige = Color(0xFFE9D8B4)
+val CreamText = Color(0xFFF7EFD9)
+val AccentBrown = Color(0xFF4A352A)
