990af9dafb55d1cb36e52c069932bc6a120dd048
Felipe-Mu-Go <gonzalo.munoz.valencia@gmail.com>
2026-03-12 22:36:24 -0300

    Actualizar tipografía global a Poppins en Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.ui:ui-text-google-fonts")
+    implementation("com.google.android.gms:play-services-base:18.5.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
